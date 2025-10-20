dda66edb171e1a96b5bc2eaaff2424750c294c21
tnedel <tnedel@f2r4s10.clusters.42paris.fr>
2025-10-20 15:06:05 +0200

    request cout

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -2,16 +2,20 @@
 # define REQUEST_HPP
 
 #include "all.hpp"
+#include "Client.hpp"
 
 class Request
 {
 private:
 	std::map<std::string, std::string> _spec;
+	std::string http;
 public:
-	Request( const char *req );
+	Request( Client& cli );
 	Request( const Request& cpy );
 	Request& operator=( const Request& other );
 	~Request();
+
+	std::map<std::string, std::string> getContainer() const;
 };
 
 std::ostream& operator<<( std::ostream& flux, const Request& r );
